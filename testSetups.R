3d4a30fb6c6c15b1baa7c3b64e1ec42e2bdad43a
wistubaT <90553418+wistubaT@users.noreply.github.com>
2023-03-26 23:26:57 +0200

    Update testSetups.R

--- testSetups.R
+++ testSetups.R
@@ -7,12 +7,15 @@ source("Methods/makeDataset.R")
 source("Methods/simulatePerformance.R")
 options(mboost_dftraceS = FALSE)
 
+# Creating a vector that contains the names of all setup files
 fileNames <- list.files("Setups/")
+# Removing the default setup file for creating new setups from the vector
 fileNames <- fileNames[-1]
 
 currProc <- 1
 totProcesses <- 0
 
+# Determining the number of setups that still need to be evaluated
 for(k in fileNames){
   if(!file.exists(paste0("Results/", substring(k, 1, nchar(k)-4), "_Standard.RData"))){
     totProcesses <- totProcesses + 1
@@ -21,7 +24,9 @@ for(k in fileNames){
 
 for(i in 1:length(fileNames)){
   k <- fileNames[i]
+  # Only running the simulations for which no results are available in the folder "Results" yet
   if(!file.exists(paste0("Results/", substring(k, 1, nchar(k)-4), "_Standard.RData"))){
+    # Reading and processing of the information in the setup files
     tempSetup <- read.csv2(paste0("Setups/", k), header = T)
     nSlopes <- as.numeric(tempSetup[1,1])
     beta <- as.numeric(tempSetup[,2])[!is.na(as.numeric(tempSetup[,2]))]
@@ -41,6 +46,7 @@ for(i in 1:length(fileNames)){
       funs <- NULL
     }
     
+    # Calling the function simulate.performance to run the simulations and save the results
     simulate.performance(nSlopes = nSlopes, beta = beta, nObs_fit = nObs_fit,
                                   nObs_test = nObs_test, funs = funs, se = se, nDatasets = nDatasets,
                                   maxIter = maxIter, filename = substring(k, 1, nchar(k)-4),
